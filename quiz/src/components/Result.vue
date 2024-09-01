eae911d06574d4cb391474de036c467dc6f37d39
Allan Lin <allanlinemail@gmail.com>
2024-09-01 19:42:48 +0800

    docs: Initial project

---
+++ quiz/src/components/Result.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="result">
+        <div class="title">{{ results[resultIndex].title }}</div>
+        <div class="desc">
+            {{ results[resultIndex].desc }}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Result',
+    props: ["results", "totalCorrect"],
+    computed: {
+        resultIndex() {
+            let r_index = 0;
+            this.results.forEach((element, index) => {
+                if (element.min <= this.totalCorrect && element.max >= this.totalCorrect) {
+                    r_index = index;
+                }
+            });
+            return r_index;
+        }
+    }
+}
+</script>
